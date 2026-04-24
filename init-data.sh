efd475e365bf656c64decd9d955034c21bc8ce47
Brian Aydemir <baydemir@morgridge.org>
2026-04-24 18:09:16 -0500

    Update the out-of-the-box defaults to assume multi-origin namespaces work

--- init-data.sh
+++ init-data.sh
@@ -17,13 +17,11 @@ set +x
 
 echo "Creating objects..."
 
-for x in {0..0}; do
+for x in {0..1}; do
   mkdir -p "data/origin/$x/data"
   for y in {0..4095}; do echo "$x.$y.$RANDOM" > "data/origin/$x/data/$x.$y"; done
   echo "Created $(((x + 1) * 4096))"
 done
-
-for x in {1..7}; do ln -s "0" "data/origin/$x"; done
 
 
 #---------------------------------------------------------------------------
@@ -33,18 +31,21 @@ echo "Creating transfer ads..."
 
 
 # Scenario: The namespace is public, and all objects exist.
+# shellcheck disable=SC2034
 for x in {0..2047}; do y="$RANDOM"; for z in {0..3}; do
   echo "[ Url=\"pelican://director:8444/public/data/0.$((RANDOM / 32))\"; LocalFileName=\"/dev/null\" ]" >>"data/ads/input/$x.$y.exist"
 done; done
 
 
 # Scenario: The namespace requires authorization, and all objects exist.
+# shellcheck disable=SC2034
 for x in {0..2047}; do y="$RANDOM"; for z in {0..1}; do
   echo "[ Url=\"pelican://director:8444/private/data/0.$((RANDOM / 64))\"; LocalFileName=\"/dev/null\" ]" >>"data/ads/input/$x.$y.token"
 done; done
 
 
 # Scenario: The namespace is public, but no objects exist.
+# shellcheck disable=SC2034
 for x in {0..2047}; do y="$RANDOM"; for z in {0..0}; do
   echo "[ Url=\"pelican://director:8444/public/data/9.$((RANDOM / 128))\"; LocalFileName=\"/dev/null\" ]" >>"data/ads/input/$x.$y.dne"
 done; done
