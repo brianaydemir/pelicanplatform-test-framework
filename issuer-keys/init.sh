72761f46d259583833da7d7787f5c900ff98251f
Chenglong Yu <88380000+melody0123@users.noreply.github.com>
2026-08-13 15:46:38 -0500

    Fix multiple init.sh script in subfolders to make the reset.sh work again
    - bin/init.sh: Switch to pelican-server binary for Mac, and update version to 7.26.0
    - certs/init.sh: Add -CAcreateserial to the signing command
    - issuer-keys/init.sh: Use relative path to invoke pelican-server binary

--- issuer-keys/init.sh
+++ issuer-keys/init.sh
@@ -9,6 +9,6 @@ rm -f ./*.jwks ./*.pem
 # Create service-specific key pairs.
 
 for svc in director registry origin cache; do
-  pelican key create --public-key "${svc}.jwks" --private-key "${svc}.pem"
+  ../bin/pelican-server key create --public-key "${svc}.jwks" --private-key "${svc}.pem"
 done
 chmod a-w ./*.jwks ./*.pem
